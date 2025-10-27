089af9feb20f3b3f6a3efdb9c7c8a8347bc8186b
richiii999 <richiii@umich.edu>
2025-10-27 19:32:49 -0400

    proper boat controls

--- VR Demo/Assets/Scripts/BoatControls.cs
+++ VR Demo/Assets/Scripts/BoatControls.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class BoatControls : MonoBehaviour{
     Rigidbody boat;
-    public float strength = 0.03f;
+    public float strength = 0.05f;
     public float maxVel = 2f;
 
     void Start() {boat=GetComponent<Rigidbody>();}
 
     void Update(){
-        boat.AddForce(Vector3.left    * strength * Input.GetAxis("Vertical"),   ForceMode.Impulse);
-        boat.AddForce(Vector3.forward * strength * Input.GetAxis("Horizontal"), ForceMode.Impulse);
+        // Go forward / backward or left / right (turning) relative to the boat's current direction
+        boat.AddForce(-transform.right * strength * Input.GetAxis("Vertical"),   ForceMode.Impulse);
+        boat.AddForce(Vector3.Normalize(Quaternion.Euler(0,-45,0) * transform.forward) * strength * Input.GetAxis("Horizontal"), ForceMode.Impulse);
 
         boat.linearVelocity = Vector3.ClampMagnitude(boat.linearVelocity, maxVel);
     }
