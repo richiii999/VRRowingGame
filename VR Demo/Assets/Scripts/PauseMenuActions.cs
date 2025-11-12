f9d2901a5868b027328159c8dc5c4fa3d8d532b9
Brazill <mbrazill@UMICH.EDU>
2025-11-12 12:05:45 -0500

    more buttons

--- VR Demo/Assets/Scripts/PauseMenuActions.cs
+++ VR Demo/Assets/Scripts/PauseMenuActions.cs
@@ -1,7 +1,16 @@
+using System.Diagnostics;
 using UnityEngine;
 
 public class PauseMenuActions : MonoBehaviour {
-    public void todo() {
-        
+    public void recalibrate() {
+        Debug.log("Recalibrate Clicked");
+    }
+    
+    public void restartLevel() {
+        Debug.log("Restart Level Clicked");
+    }
+
+    public void returnToMainMenu() {
+        Debug.log("Return to Main Menu Clicked");
     }
 }
