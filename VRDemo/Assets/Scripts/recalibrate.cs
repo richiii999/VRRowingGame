00008e8925c0f1bdfc42e886afb7319ce8f25468
HJOHN2 <122955690+HJOHN2@users.noreply.github.com>
2026-01-14 16:00:48 -0500

    Camera Calibration WIP
    Need to attach recalibrate to hands and also move the player relative to the boat using MoveCameraToWorldPostion

--- VRDemo/Assets/Scripts/recalibrate.cs
+++ VRDemo/Assets/Scripts/recalibrate.cs
@@ -1,13 +1,15 @@
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class recalibrate : MonoBehaviour
 {
+    public GameObject boat;
+    public Component xrOrigin;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        this.transform.localPosition = new Vector3((float)0.0, (float)0.0, (float)0.0);
+        xrOrigin.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3((float) 0.0,(float) 1.2,(float) 0.0));
     }
-
     // Update is called once per frame
     void Update()
     {
