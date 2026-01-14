505555dacbc167c8924b8d8f925d8dc98a1ead19
HJOHN2 <122955690+HJOHN2@users.noreply.github.com>
2026-01-14 13:21:26 -0500

    Compiled Build

--- VRDemo/Assets/recalibrate.cs
+++ VRDemo/Assets/recalibrate.cs
@@ -5,7 +5,7 @@ public class recalibrate : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        this.transform.localPosition = new Vector3((float)-0.011, (float)0.8, (float)-1.83);
+        this.transform.localPosition = new Vector3((float)0.0, (float)0.0, (float)0.0);
     }
 
     // Update is called once per frame
