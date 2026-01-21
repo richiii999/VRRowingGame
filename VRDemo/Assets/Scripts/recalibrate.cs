dc4560e7d3ca73e59f9b87d3d92dbb362a572ed8
HJOHN2 <122955690+HJOHN2@users.noreply.github.com>
2026-01-21 15:53:14 -0500

    Always start scene in boat. Space moves player to boat.

--- VRDemo/Assets/Scripts/recalibrate.cs
+++ VRDemo/Assets/Scripts/recalibrate.cs
@@ -6,13 +6,24 @@ public class recalibrate : MonoBehaviour
     public GameObject boat;
     public Component xrOrigin;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private int start;
     void Start()
     {
-        xrOrigin.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3((float) 0.0,(float) 1.2,(float) 0.0));
+        start = 0;
+        //xrOrigin.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3((float) (boat.transform.position.x-0.8),(float) (boat.transform.position.y+2.0),(float) (boat.transform.position.z-0.63)));
+        xrOrigin.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3((float) (boat.transform.position.x),(float) (boat.transform.position.y+2.0),(float) (boat.transform.position.z)));
     }
     // Update is called once per frame
     void Update()
     {
-        
+        start++;
+        if (start == 10)
+        {
+            xrOrigin.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3((float) (boat.transform.position.x-0.6),(float) (boat.transform.position.y+1.5),(float) (boat.transform.position.z+0.0)));
+        }
+        if ((int)Input.GetAxis("Jump") != 0)
+        {
+            xrOrigin.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3((float) (boat.transform.position.x-0.6),(float) (boat.transform.position.y+1.5),(float) (boat.transform.position.z+0.0)));
+        }
     }
 }
