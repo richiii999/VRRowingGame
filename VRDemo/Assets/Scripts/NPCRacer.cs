ff256a9fa65a5a935949e43655cd08a3f443de26
richiii999 <richiii@umich.edu>
2026-02-02 14:23:34 -0500

    NPC racer oars anim

--- VRDemo/Assets/Scripts/NPCRacer.cs
+++ VRDemo/Assets/Scripts/NPCRacer.cs
@@ -9,18 +9,19 @@ using System;
 public class NPCRacer : MonoBehaviour{
     public GameObject oarL = null; // Refs to the Oars objects (to spin them)
     public GameObject oarR = null; 
+    public GameObject lookTargetL = null;
+    public GameObject lookTargetR = null;
 
     public float speed = 1.0f; // How fast the NPC goes along their track
-
-    void Start(){
-        oarL = GameObject.Find("Oar_Left"); // Tips of oars
-        oarR = GameObject.Find("Oar_Right"); 
-    }
+    public float rowAnimSpan = 4.0f; // How wide the row anim move the oars
 
     void Update(){
-        if (oarL != null && oarR != null){
-            oarL.transform.RotateAround(oarL.transform.position, new Vector3(0,1,0), (float)Math.Sin(Time.time) * 0.5f * speed );
-            oarR.transform.RotateAround(oarR.transform.position, new Vector3(0,1,0), (float)Math.Sin(Time.time) * 0.5f * speed * -1);
+        if (oarL != null && oarR != null){ // Make the oars "row" in a loop by pointing towards moving targets
+            oarL.transform.LookAt(lookTargetL.transform);
+            oarR.transform.LookAt(lookTargetR.transform);
+
+            lookTargetL.transform.position += new Vector3(0f, 0f, (float)Math.Sin(Time.time) * rowAnimSpan);
+            lookTargetR.transform.position += new Vector3(0f, 0f, (float)Math.Sin(Time.time) * rowAnimSpan * -1);
         }
         
     }
