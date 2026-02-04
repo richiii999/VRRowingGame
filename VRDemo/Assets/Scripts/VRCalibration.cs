c1166c513af812237b15717f94972916b0be8345
richiii999 <richiii@umich.edu>
2026-02-04 16:05:30 -0500

    VRCalibrate omg bruh

--- VRDemo/Assets/Scripts/VRCalibration.cs
+++ VRDemo/Assets/Scripts/VRCalibration.cs
@@ -1,18 +1,17 @@
-using System.Collections;
+using Unity.XR.CoreUtils;
 using UnityEngine;
+using System.Collections;
 
-public class VRCalibration : MonoBehaviour{
-    public Transform calibrationObj; // Snap camera to this Obj's position
+public class recalibrate : MonoBehaviour{
+    public Transform boat;
+    public XROrigin xrOrigin;
+    public Vector3 offset;
     public KeyCode key;
-    public Vector3 positionOffset;
 
     void Start(){ StartCoroutine(LateStart(0.5f)); } // Delay starting calibration by a short time
     IEnumerator LateStart(float waitTime){yield return new WaitForSeconds(waitTime); Calibrate(); }
 
     void Update(){ if (Input.GetKeyDown(key)) Calibrate(); }
 
-    void Calibrate() { 
-        transform.position = calibrationObj.position + positionOffset; 
-        transform.rotation = calibrationObj.rotation;
-    }
+    void Calibrate(){ xrOrigin.MoveCameraToWorldLocation(boat.position + offset); }
 }
