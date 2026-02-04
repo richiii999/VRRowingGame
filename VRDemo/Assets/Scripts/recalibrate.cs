73389af0f8aa1dbfd4b635d7b0a5359f287204e8
richiii999 <richiii@umich.edu>
2026-02-04 15:08:19 -0500

    VR Recalibrate function fix

--- VRDemo/Assets/Scripts/recalibrate.cs
+++ VRDemo/Assets/Scripts/recalibrate.cs
@@ -1,30 +1,17 @@
+using System.Collections;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class recalibrate : MonoBehaviour
 {
     public GameObject boat;
-    public Component xrOrigin;
-    public float xOffset;
-    public float yOffset;
-    public float zOffset;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private int start;
-    void Start()
-    {
-        start = 0;
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        start++;
-        if (start == 10)
-        {
-            xrOrigin.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3((float) (boat.transform.position.x+xOffset),(float) (boat.transform.position.y+yOffset),(float) (boat.transform.position.z+zOffset)));
-        }
-        if ((int)Input.GetAxis("Jump") != 0)
-        {
-            xrOrigin.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3((float) (boat.transform.position.x+xOffset),(float) (boat.transform.position.y+yOffset),(float) (boat.transform.position.z+zOffset)));
-        }
-    }
+    public XROrigin xrOrigin;
+    public Vector3 offset;
+
+    void Start(){ StartCoroutine(LateStart(0.5f)); } // Delay starting calibration by a short time
+    IEnumerator LateStart(float waitTime){yield return new WaitForSeconds(waitTime); Recalibrate(); }
+
+    void Update(){ if (Input.GetKeyDown(KeyCode.Space)) Recalibrate(); }
+
+    void Recalibrate() { xrOrigin.GetComponent<XROrigin>().MoveCameraToWorldLocation(boat.transform.position + offset); }
 }
