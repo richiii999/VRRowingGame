aa2c12509c3d8d2d48fb3bd2ac2080c975da66ea
richiii999 <richiii@umich.edu>
2026-02-11 14:44:08 -0500

    BoatUI retry button added

--- VRDemo/Assets/Scripts/BoatUI.cs
+++ VRDemo/Assets/Scripts/BoatUI.cs
@@ -4,30 +4,37 @@ using TMPro;
 // BoatUI: Contains funcs for activating the boatUI buttons and win/lose text.
 
 public class BoatUI : MonoBehaviour{
-    public TextMeshPro timerText = null;
+    public TextMeshProUGUI timerText = null; // Refs to child objects
+    public GameObject menuButton = null;
+    public GameObject nextButton = null;
+    public GameObject retryButton = null;
+    
     
     void Start(){
-        timerText.text = "Pass the Checkpoint to start!";
+        if (timerText == null || menuButton == null || nextButton == null || retryButton == null) Debug.LogError("BoatUI missing child connections!");
         
+        timerText.text = "Pass the Checkpoint to start!";
+        menuButton.SetActive(false);
+        nextButton.SetActive(false);
+        retryButton.SetActive(false);
     }
 
     public void SetTimerText(float t = 0.0f){
-        if (t != 0.0f) timerText.text = "Time: " + (t).ToString("F1");
+        if (t != 0.0f) timerText.text = "Time: " + t.ToString("F1");
     }
 
     public void FinishButton(bool win = true){ // Activate next level (true) / retry (false) button, and the main menu. Called from outside on level finish
-        transform.Find("Canvas/FinishGroup/MenuButton").gameObject.SetActive(true); 
+        menuButton.SetActive(true); 
 
         if (win){
-            transform.Find("Canvas/FinishGroup/NextLevelButton").gameObject.SetActive(true); 
+            nextButton.SetActive(true); 
             timerText.text += ", You Win!";
             timerText.color = Color.yellow;
         }
         else{
-            transform.Find("Canvas/FinishGroup/RetryButton").gameObject.SetActive(true);
+            retryButton.SetActive(true);
             timerText.text += ", You Lose!";
             timerText.color = Color.red;
-
         }
     }
 }
