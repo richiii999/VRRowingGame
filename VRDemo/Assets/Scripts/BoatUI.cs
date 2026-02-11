ea5ad451e968a1df39cf9456785d14620a2f13f4
richiii999 <richiii@umich.edu>
2026-02-11 15:55:08 -0500

    boatUI anchors

--- VRDemo/Assets/Scripts/BoatUI.cs
+++ VRDemo/Assets/Scripts/BoatUI.cs
@@ -4,25 +4,37 @@ using TMPro;
 // BoatUI: Contains funcs for activating the boatUI buttons and win/lose text.
 
 public class BoatUI : MonoBehaviour{
-    public TextMeshProUGUI timerText = null; // Refs to child objects
+    public Transform boatFrontUIAnchor = null; // refs to boat UI anchors (on boat prefab)
+    public Transform boatBackUIAnchor = null;
+    public GameObject rayToEnableOnFinish = null; // On parent boatObject
+
+    public TextMeshProUGUI timerTextFront = null; // Refs to child objects
+    public TextMeshProUGUI timerTextBack = null;
     public GameObject menuButton = null;
     public GameObject nextButton = null;
     public GameObject retryButton = null;
-    public GameObject rayToEnableOnFinish = null; // On parent boatObject
-    
     
     void Start(){
-        if (timerText == null || menuButton == null || nextButton == null || retryButton == null || rayToEnableOnFinish == null) Debug.LogError("BoatUI a reference!");
-        
-        timerText.text = "Pass the Checkpoint to start!";
+        timerTextFront.text = "Pass the Checkpoint to start!";
+        timerTextBack.text = "Pass the Checkpoint to start!";
         menuButton.SetActive(false);
         nextButton.SetActive(false);
         retryButton.SetActive(false);
         rayToEnableOnFinish.SetActive(false);
+
+
+        Vector3 UIScale = transform.Find("FrontCanvas").localScale; // Preserve UI scale when reparenting
+        boatFrontUIAnchor.localScale = UIScale;
+        boatBackUIAnchor.localScale = UIScale;
+        transform.Find("FrontCanvas").transform.SetParent(boatFrontUIAnchor, false); // Reparent UI canvases to be in correct positions on boat
+        transform.Find("BackCanvas").transform.SetParent(boatBackUIAnchor, false);
     }
 
     public void SetTimerText(float t = 0.0f){
-        if (t != 0.0f) timerText.text = "Time: " + t.ToString("F1");
+        if (t != 0.0f) { 
+            timerTextFront.text = "Time: " + t.ToString("F1"); 
+            timerTextBack.text  = "Time: " + t.ToString("F1"); 
+        }
     }
 
     public void FinishButton(bool win = true){ // Activate next level (true) / retry (false) button, and the main menu & ray. Called from outside on level finish
@@ -31,13 +43,17 @@ public class BoatUI : MonoBehaviour{
 
         if (win){
             nextButton.SetActive(true); 
-            timerText.text += "\nYou Win!";
-            timerText.color = Color.yellow;
+            timerTextFront.text += "\nYou Win!";
+            timerTextFront.color = Color.yellow;
+            timerTextBack.text += "\nYou Win!";
+            timerTextBack.color = Color.yellow;
         }
         else{
             retryButton.SetActive(true);
-            timerText.text += "\nYou Lose!";
-            timerText.color = Color.red;
+            timerTextFront.text += "\nYou Lose!";
+            timerTextFront.color = Color.red;
+            timerTextBack.text += "\nYou Lose!";
+            timerTextBack.color = Color.red;
         }
     }
 }
