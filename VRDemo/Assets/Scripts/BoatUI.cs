de33ea41973baea9b4d9d4bf18d6c2bdc43c1813
richiii999 <richiii@umich.edu>
2026-02-11 14:04:07 -0500

    BoatUI changes WIP

---
+++ VRDemo/Assets/Scripts/BoatUI.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+// BoatUI: Contains funcs for activating the boatUI buttons and win/lose text.
+
+public class BoatUI : MonoBehaviour{
+    
+    void Start(){
+        
+    }
+
+    void Update(){
+        
+    }
+}
