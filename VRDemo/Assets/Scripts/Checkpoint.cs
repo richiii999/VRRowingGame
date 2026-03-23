091acb7350c34e14ed6821524aa64f109921cc43
Maverick <mbrazill@umich.edu>
2026-03-23 13:13:05 -0400

    Fixed Checkpoint alpha and color; fixed 'retry level' button crash on TestLevel

--- VRDemo/Assets/Scripts/Checkpoint.cs
+++ VRDemo/Assets/Scripts/Checkpoint.cs
@@ -3,6 +3,9 @@ using System;
 using TMPro;
 
 using static Tools;
+using System.Drawing;
+using Color = UnityEngine.Color;
+
 
 // Checkpoint.cs: Signals the parent CheckpointController when a CP is crossed by a Player/NPC
 
@@ -25,26 +28,43 @@ public class Checkpoint : MonoBehaviour{
         // set ref
         CPC = RefToComp<CheckpointController>("CheckpointGroup");
 
-        SetGlowAlpha(0f); // Hide checkpoint glow on start
+        SetGlowAlpha(0.15f); // Super low checkpoint glow on start
     }
 
     void Update(){ 
         if (isNext) { 
-            SetGlowAlpha( Mathf.Abs(((float)Math.Sin(Time.time)) * 0.7f) );
+            // Minimum value 0.1 - PingPong between 0.1-0.5
+            // Multiply Time by 0.4 to slow the PingPong down a little bit
+            float alpha = Mathf.PingPong(Time.time * 0.4f, 0.6f) + 0.1f;
+            SetGlowAlpha(alpha);
             
             if (startTime == 0.00f) startTime = Time.time; // First CP doesnt count timer
             timerTxt.text = (Time.time - startTime).ToString("F1"); // Round time to 2 dec places
         }
-
     }
 
     // Signal Player collisions to CheckpointController
     void OnTriggerEnter(Collider other){ 
         if ( !(isNext && other.CompareTag("Player")) ) return; // Player, in-order only
+        DeactivateCheckpoint(other);
+    }
+
+    public void SetGlowAlpha(float a){
+        // Same Color as whatever the post is right now, just with an updated alphaw
+        SetColor(new Color(PostL.material.color.r, PostL.material.color.g, PostL.material.color.b, a));
+    }
+
+    private void SetColor(Color color) {
+        PostL.material.color = color;
+        PostR.material.color = color;
+    }
+
+    private void DeactivateCheckpoint(Collider other) {
+        if ( !(isNext && other.CompareTag("Player")) ) return; // Player, in-order only
         
         CPC.OnCheckpoint(this); 
         other.GetComponentInChildren<BoatUI>().Score( scoreTime / (Time.time - startTime), CPC.ResetMaxAngle()); // ResetMaxAngle() returns the value it was.
+        SetColor(Color.gray);
+        SetGlowAlpha(0.4f);
     }
-
-    public void SetGlowAlpha(float a){ if (PostL) PostL.material.color = new Color( PostL.material.color.r, PostL.material.color.g, PostL.material.color.b, a); }
 }
