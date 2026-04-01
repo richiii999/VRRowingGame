74f6117b7ba7ab169cfe410386f5ed4ba454c47f
richiii999 <richiii@umich.edu>
2026-04-01 16:12:43 -0400

    testing varrs

--- VRDemo/Assets/Scripts/UI/MenuActions.cs
+++ VRDemo/Assets/Scripts/UI/MenuActions.cs
@@ -20,12 +20,15 @@ public class MenuActions : MonoBehaviour {
     public Vector3 offset;
     void Start() { ToggleActive(false); } // Hidden by default (leave active in editor)
 
+    public string Axisname = "XRI_Left_Trigger";
+    public float threshHold = 0.5f;
+
     void Update() {
         if (Input.GetKeyDown(KeyCode.Escape)) {
             if (GameIsPaused) Resume();
             else Pause();
         }
-        if (Input.GetAxisRaw("XRI_Left_Trigger") > .5 && !GameIsPaused) Pause();
+        if (Input.GetAxisRaw(Axisname) > threshHold && !GameIsPaused) Pause();
     }
 
     void ToggleActive(bool state){ // True means pause menu is shown, false means it is hidden
