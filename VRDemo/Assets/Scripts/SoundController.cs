fd764a6b076dd8719eb75207c83f4d72882fa713
richiii999 <richiii@umich.edu>
2026-04-06 13:30:15 -0400

    remove debug print

--- VRDemo/Assets/Scripts/SoundController.cs
+++ VRDemo/Assets/Scripts/SoundController.cs
@@ -53,7 +53,8 @@ public class SoundController : MonoBehaviour{
     // BUG (bad design): why cant I have a Vector3(0,0,0) as default param? So dumb to separate it to xyz
     public void PlayRandomSound(string category = "none", float x = 0f, float y = 0f, float z = 0f){ 
         currSound = null; // Reset currSound to prevent double play of sound effects
-        Debug.Log($"Sound played, cat={category}");
+        // Debug.Log($"Sound played, cat={category}");
+        
         switch (category){
             case "splash": 
                 if (splashTimer == 0f) {
