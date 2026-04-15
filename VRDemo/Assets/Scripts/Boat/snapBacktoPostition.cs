929e8a81d986430af6f58ed25ce1e39ea0aa3c52
HJOHN2 <122955690+HJOHN2@users.noreply.github.com>
2026-04-15 13:30:50 -0400

    Updated Canal, Difficulty options, and oar snap back now happens on a timer

--- VRDemo/Assets/Scripts/Boat/snapBacktoPostition.cs
+++ VRDemo/Assets/Scripts/Boat/snapBacktoPostition.cs
@@ -4,7 +4,23 @@ using UnityEngine;
 public class snapBacktoPostition : MonoBehaviour
 {
     public GameObject oar;
+    private double timePassed = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        if(timePassed > 0.1)
+        {
+            SetRotation();
+            timePassed -= 0.1;
+        }
+    }    
     public void SetRotation()
     {
         transform.localRotation = Quaternion.Euler(42.297f, 90f, 0f);
